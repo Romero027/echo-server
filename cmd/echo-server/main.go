dcb40437329f8ec1c8ff9aad2f36d47c3b0b4df8
Xiangfeng Zhu <xzhu0027@gmail.com>
2022-04-14 19:09:31 -0700

    Update main.go

--- cmd/echo-server/main.go
+++ cmd/echo-server/main.go
@@ -46,11 +46,11 @@ var upgrader = websocket.Upgrader{
 func handler(wr http.ResponseWriter, req *http.Request) {
 	defer req.Body.Close()
 
-	if os.Getenv("LOG_HTTP_BODY") != "" || os.Getenv("LOG_HTTP_HEADERS") != "" {
-		fmt.Printf("--------  %s | %s %s\n", req.RemoteAddr, req.Method, req.URL)
-	} else {
-		fmt.Printf("%s | %s %s\n", req.RemoteAddr, req.Method, req.URL)
-	}
+// 	if os.Getenv("LOG_HTTP_BODY") != "" || os.Getenv("LOG_HTTP_HEADERS") != "" {
+// 		fmt.Printf("--------  %s | %s %s\n", req.RemoteAddr, req.Method, req.URL)
+// 	} else {
+// 		fmt.Printf("%s | %s %s\n", req.RemoteAddr, req.Method, req.URL)
+// 	}
 
 	if os.Getenv("LOG_HTTP_HEADERS") != "" {
 		fmt.Printf("Headers\n")
